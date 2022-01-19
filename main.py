b97ecc813f729abc1de906253a5c7084fc569f02
cube-user <ilay.k.208@skam.ru>
2022-01-19 17:28:31 +0500

    NO F#CK YOU!!!!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ name = "Сидорович"
 bully = name + "Boy next door"
 bully2 = "hotlos"
 bulle3 = "Sorry for what?"
+bulle3_power = "300$"
+bully5 ="F#ck yoou"
 
 for i in range(10):
-    print(bully, bulle3, bully2)
+    print(bully, bulle3, bully2, bulle3_power)
